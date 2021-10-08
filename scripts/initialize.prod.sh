fa7b37e1971e08c55e7a10ecedf6150f2d3f793f
Justin Taylor <709905+kydreth@users.noreply.github.com>
2021-10-07 20:25:49 -0400

    update to debian host, misc bug fixes

--- scripts/initialize.prod.sh
+++ scripts/initialize.prod.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
 
-#setup centos 7.x AS ROOT
-yum update -y -q -e 0
-yum install -y -q -e 0 git vim wget pushd popd bzip2 zip unzip nano less dnsutils nc telnet yum-utils
-yum clean all
+#setup debian 10 buster AS ROOT
+apt-get update -y -q -e 0
+apt-get install -y -q -e 0 curl git vim wget bzip2 zip unzip nano less dnsutils telnet yum-utils
+apt-get clean all
 
 #setup docker AS ROOT
-yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-yum install -y -q -e 0 docker-ce docker-ce-cli containerd.io
-usermod -aG docker centos
-systemctl enable docker
-systemctl start docker
-curl -L -s https://github.com/docker/compose/releases/download/1.25.1-rc1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose > /dev/null
+apt-get install -y -q -e 0 apt-transport-https ca-certificates gnupg lsb-release
+curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update -y -q -e 0
+apt-get install -y -q -e 0 docker-ce docker-ce-cli containerd.io
+curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
-docker network create -d bridge web
-docker run -d -p 5000:5000 --restart=always --name registry registry:latest
+#usermod -aG docker $USER
+#docker run -d -p 5000:5000 --restart=always --name registry registry:latest
 
 #setup codebase as ROOT
 cd /opt
 git clone https://github.com/kydreth/postexample.git
+cd postexample
 docker build -t centos/api api
 docker build -t centos/web web
 docker build -t centos/nginx nginx
